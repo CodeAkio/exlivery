86ab0a6ae1c1b6a2f3c71b4755b0fdbdf0baf9de
CodeAkio <siqueira_2004@hotmail.com>
2021-09-23 18:44:19 -0300

    Change UserAgent to start with a default value

--- lib/users/agent.ex
+++ lib/users/agent.ex
@@ -3,8 +3,8 @@ defmodule Exlivery.Users.Agent do
 
   alias Exlivery.Users.User
 
-  def start_link(_initial_state) do
-    Agent.start_link(fn -> %{} end, name: __MODULE__)
+  def start_link(initial_state \\ %{}) do
+    Agent.start_link(fn -> initial_state end, name: __MODULE__)
   end
 
   def save(%User{} = user) do
