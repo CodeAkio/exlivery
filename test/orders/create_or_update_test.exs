6fc6ca0e6bd4c3d050a7020e0347d1094f0bf2de
CodeAkio <siqueira_2004@hotmail.com>
2021-09-24 23:33:01 -0300

    Test for create or update order

---
+++ test/orders/create_or_update_test.exs
@@ -0,0 +1,80 @@
+defmodule Exlivery.Orders.CreateOrUpdateTest do
+  use ExUnit.Case
+
+  import Exlivery.Factory
+
+  alias Exlivery.Orders.CreateOrUpdate
+  alias Exlivery.Users.Agent, as: UserAgent
+
+  describe "call/1" do
+    setup do
+      Exlivery.start_agents()
+
+      cpf = "111.111.111-11"
+      user = build(:user, cpf: cpf)
+      UserAgent.save(user)
+
+      item1 = %{
+        description: "Pizza de frango",
+        category: :pizza,
+        unity_price: "35.50",
+        quantity: 1
+      }
+
+      item2 = %{
+        description: "Acai",
+        category: :sobremesa,
+        unity_price: "15.00",
+        quantity: 1
+      }
+
+      {:ok, user_cpf: cpf, item1: item1, item2: item2}
+    end
+
+    test "when all params are valid, saves the order", %{
+      user_cpf: cpf,
+      item1: item1,
+      item2: item2
+    } do
+      params = %{user_cpf: cpf, items: [item1, item2]}
+
+      response = CreateOrUpdate.call(params)
+
+      assert {:ok, _uuid} = response
+    end
+
+    test "when there is no user with given cpf, returns an error", %{item1: item1, item2: item2} do
+      params = %{user_cpf: "000.000.000-00", items: [item1, item2]}
+
+      response = CreateOrUpdate.call(params)
+
+      expected_response = {:error, "User not found"}
+
+      assert response == expected_response
+    end
+
+    test "when there are invalid itens, returns an error", %{
+      user_cpf: cpf,
+      item1: item1,
+      item2: item2
+    } do
+      params = %{user_cpf: cpf, items: [%{item1 | quantity: 0}, item2]}
+
+      response = CreateOrUpdate.call(params)
+
+      expected_response = {:error, "Invalid items"}
+
+      assert response == expected_response
+    end
+
+    test "when there are no itens, returns an error", %{user_cpf: cpf} do
+      params = %{user_cpf: cpf, items: []}
+
+      response = CreateOrUpdate.call(params)
+
+      expected_response = {:error, "Invalid params"}
+
+      assert response == expected_response
+    end
+  end
+end
